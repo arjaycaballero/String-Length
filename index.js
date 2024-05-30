5e8c3253a97268b580f1dc83f4cf35705e16f7c9
Arjay Caballero <130941463+arjaycaballero@users.noreply.github.com>
2024-05-30 21:15:13 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var tweet = prompt("Compose you tweet:");
+var tweetCount = tweet.length;
+alert("You have written " + tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining.");
+
+
+
+// You have written 182 characters, you have -42 characters left.
